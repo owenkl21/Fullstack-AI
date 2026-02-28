a053fa54e31925b424362c1888aaf99269f8b9d1
Owen Kleinhans <92853486+owenkl21@users.noreply.github.com>
2026-02-28 18:53:57 +0200

    [Update] refactored the backend for now

---
+++ packages/server/controllers/chat.controller.ts
@@ -0,0 +1,32 @@
+import type { Request, Response } from 'express';
+import { chatService } from './../services/chat.service';
+import z from 'zod';
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt must be less than 1000 characters.'),
+   conversationId: z.string().uuid(),
+});
+
+export const chatController = {
+   sendMessage: async (req: Request, res: Response) => {
+      const parseResult = chatSchema.safeParse(req.body);
+      if (!parseResult.success) {
+         res.status(400).json(parseResult.error.format());
+      }
+
+      try {
+         const { prompt, conversationId } = req.body;
+         const response = await chatService.sendMessage(prompt, conversationId);
+
+         res.json({ message: response.message });
+      } catch (error) {
+         res.status(500).json({
+            error: 'An error occurred while processing your request.',
+         });
+      }
+   },
+};
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,17 @@
 import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import router from './routes';
 
 //reads variables from .env file and adds them to process.env
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello from the API!' });
-});
 
 app.listen(port, () => {
    console.log(`Server is running on port http://localhost:${port}`);
