301daff80f16873d4904cba95554157654c5c457
Owen Kleinhans <92853486+owenkl21@users.noreply.github.com>
2026-02-28 14:52:31 +0200

    [Update] connect front and back end

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -13,6 +13,11 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({message: 'Hello from the API!'});
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
 })
