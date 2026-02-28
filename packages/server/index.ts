82eb37be415f296d7649846567ddae9fb1087e37
Owen Kleinhans <92853486+owenkl21@users.noreply.github.com>
2026-02-28 15:38:45 +0200

    [update] set up prettier rules

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,23 +1,21 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 //reads variables from .env file and adds them to process.env
 dotenv.config();
 
 const app = express();
-const port =  process.env.PORT || 3000;
-
+const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+   res.send('Hello World!');
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
-    res.json({message: 'Hello from the API!'});
+   res.json({ message: 'Hello from the API!' });
 });
 
-
 app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
-})
+   console.log(`Server is running on port http://localhost:${port}`);
+});
